30bf024ec9770db957109ff61940ab8a24ba2bb4
vuthy <vuthy104.infinity@gmail.com>
2023-12-11 17:18:13 +0700

    Init package

--- config/app.php
+++ config/app.php
@@ -194,6 +194,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Vuthy\AuthenticationManager\AuthProvider::class,
 
     ],
 
